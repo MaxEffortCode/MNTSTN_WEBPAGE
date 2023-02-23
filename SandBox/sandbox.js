7f00987a769726f927e822bebf352ab4c3081418
maxeffortcode <bultman82@gmail.com>
2023-02-23 16:41:04 -0600

    python sandbox now runs on the testing page

--- SandBox/sandbox.js
+++ SandBox/sandbox.js
@@ -1,23 +1,59 @@
-const { PythonShell } = require('python-shell');
+const { PythonShell } = require("python-shell");
+const fs = require("fs");
+const { error } = require("console");
 
-//need to fix this cehck chat gpt
-exports.pythonSandbox = async (req, res, next) => {
-    try {
-        let options = {
-        mode: 'text',
-        pythonPath: 'C:/Users/Owner/AppData/Local/Programs/Python/Python39/python.exe',
-        pythonOptions: ['-u'],
-        scriptPath: 'C:/Users/Owner/Desktop/NodeJS/NodeJS-UserNM/Sandbox',
-        args: [req.body.code]
-        };
-        PythonShell.run('sandbox.py', options, function (err, results) {
-        if (err) throw err;
-        console.log('results: %j', results);
-        res.send(results);
-        });
-    } catch (err) {
-        console.log(err);
-        res.send(err);
-    }
+function savePythonFile(input) {
+  const filename = "SandBox/UserPythonScripts/tester.py";
+  const content = input;
+
+  fs.writeFile(filename, content, (err) => {
+    if (err) throw err;
+    console.log("The file has been saved!");
+  });
 }
 
+exports.pythonSandbox = async (req, res, next) => {
+  const code = req.body.message;
+  savePythonFile(code);
+
+  try {
+    console.log("trying to run user python code: " + code);
+
+    let options = {
+      mode: "text",
+      pythonOptions: ["-u"],
+      scriptPath: "SandBox/UserPythonScripts",
+    };
+
+    const pyshell = new PythonShell("tester.py", options);
+
+    let dataToSend = "";
+
+    pyshell.on("message", (message) => {
+      console.log("result: " + message);
+      dataToSend = message;
+    });
+
+    pyshell.end((err, code, signal) => {
+      if (err) {
+        console.log("error here");
+        console.log(err);
+        //set variable responseError to the first error in the array
+        const responseError = err.stack.split("at")[0];
+        const responseErrorClean = responseError.replace("File \"SandBox/UserPythonScripts/tester.py\",", "");
+        
+        res.status(500).json({ success: false, userCodeReturn: responseErrorClean });
+        
+      } else {
+        console.log("The exit code was: " + code);
+        console.log("The exit signal was: " + signal);
+        console.log("finished");
+        res.status(200).json({ success: true, userCodeReturn: dataToSend });
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    console.log("error");
+    res.status(500).json({ success: false, error: err });
+  }
+};
